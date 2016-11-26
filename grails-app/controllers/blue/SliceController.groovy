c075ae4b7a831b7b7e42cd6396e5181db0efb963
EC2 Default User <ec2-user@ip-172-31-37-80.us-west-2.compute.internal>
2016-11-26 22:31:46 +0000

    Inch toward jsonSlurper username password

--- grails-app/controllers/blue/SliceController.groovy
+++ grails-app/controllers/blue/SliceController.groovy
@@ -30,13 +30,11 @@ String user= env['USERNAME']
 	def staticAuthor = env.grep{it.key=='VCAP_SERVICES'} //['alabama','georgia','florida'] //'Anonymous'
 	def staticContent = 'Red Sky at Dawn'
 //[{"user-provided":[{ "credentials": { "password": "judy", "username": "craig" }, "syslog_drain_url": "", "volume_mounts": [ ], "label": "user-provided", "name": "myups", "tags": [ ] }]}]
-	//fails: [ author: staticAuthor.value[0]["user-provided"], content: staticContent ]
-	//fails: [ author: staticAuthor.value.grep{it.key=='user-provided'}, content: staticContent ]
 	//this is a string:
 	//{"user-provided":[{ "credentials": { "password": "judy", "username": "craig" }, "syslog_drain_url": "", "volume_mounts": [ ], "label": "user-provided", "name": "myups", "tags": [ ] }]}
-def jsonSlurper = new JsonSlurper()
-def object = jsonSlurper.parseText(staticAuthor.value[0])
-println object.cluster_name
-	[ author: object."user-provided", content: staticContent ]
+	def jsonSlurper = new JsonSlurper()
+	def object = jsonSlurper.parseText(
+	 staticAuthor.value[0]."user-provided"[0]."credentials")
+	[ author: object, content: staticContent ]
     }
 }
