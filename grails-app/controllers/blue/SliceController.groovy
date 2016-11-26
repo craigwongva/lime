b43fc2fa086cd6f792b5d1c8324acf8111be9fef
EC2 Default User <ec2-user@ip-172-31-37-80.us-west-2.compute.internal>
2016-11-26 21:26:13 +0000

    Fix value[0]."user-provided"

--- grails-app/controllers/blue/SliceController.groovy
+++ grails-app/controllers/blue/SliceController.groovy
@@ -26,6 +26,6 @@ String user= env['USERNAME']
 	def staticAuthor = env.grep{it.key=='VCAP_SERVICES'} //['alabama','georgia','florida'] //'Anonymous'
 	def staticContent = 'Red Sky at Dawn'
 //[{"user-provided":[{ "credentials": { "password": "judy", "username": "craig" }, "syslog_drain_url": "", "volume_mounts": [ ], "label": "user-provided", "name": "myups", "tags": [ ] }]}]
-	[ author: staticAuthor.value[0]."user-provided", content: staticContent ]
+	[ author: staticAuthor.value[0]["user-provided"], content: staticContent ]
     }
 }
