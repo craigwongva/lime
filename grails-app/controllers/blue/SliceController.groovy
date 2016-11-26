88bc895cd7b071f66a63ba1664c0160ccb53d698
EC2 Default User <ec2-user@ip-172-31-32-43.us-west-2.compute.internal>
2016-11-26 04:02:51 +0000

    Improve render syntax again

--- grails-app/controllers/blue/SliceController.groovy
+++ grails-app/controllers/blue/SliceController.groovy
@@ -3,7 +3,7 @@ package blue
 class SliceController {
 
     def index() { 
-        render(model:['friend': 'Monica'])
+        render(view:'index',model:[friend: Monica])
 /*
         render(contentType: 'text/json') {[
             'dotStatus': stringOfDotStatusEachRepresentsAPiazzaJob(),
