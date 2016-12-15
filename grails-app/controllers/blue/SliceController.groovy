88b0bdc33da56394b940c4c831628c795a3cf401
EC2 Default User <ec2-user@ip-172-31-1-204.us-west-2.compute.internal>
2016-12-15 14:28:20 +0000

    Add multiregion support

--- grails-app/controllers/blue/SliceController.groovy
+++ grails-app/controllers/blue/SliceController.groovy
@@ -21,45 +21,51 @@ class SliceController {
     }
 
     def servers = {
-	def allProjectValuesAsNameCRLFValue = []
-	//The following returns a literal 'Project' on one line, then a value like 'beachfront-int' on the next line.
-        def getAllProjectValuesAsNameCRLFValue = "aws ec2 describe-instances --filter Name=tag-key,Values=Project --query Reservations[].Instances[].[Tags] --region us-west-2 --output text".execute()
-	allProjectValuesAsNameCRLFValue = getAllProjectValuesAsNameCRLFValue.text.split()
-	int allProjectValuesAsNameCRLFValueSize = allProjectValuesAsNameCRLFValue.size()
+	def regions = ['us-west-2'/*,'us-east-1'*/]
+	regions.each { region ->
 
-	def allProjectValues = []
-	for (int i=0; i<allProjectValuesAsNameCRLFValueSize; i += 2) {
-	    if (allProjectValuesAsNameCRLFValue[i] == 'Project') {
-		allProjectValues.add(allProjectValuesAsNameCRLFValue[i+1])
-	    }
-	}
-
-	//Use a list of allInstances to determine which instancs are untagged,
-	// because the CLI doesn't seem to allow the same wildcards that the EC2 Dashboard GUI filter window allows
-	// e.g. "Not tagged" in "tag:Project : Not tagged"
-	def allInstances = []
-	def allTagged = [:]
-	//The following returns one InstanceId value per line.
-	def all = "aws ec2 describe-instances --query Reservations[].Instances[].[InstanceId] --region us-west-2 --output text".execute()
-	allInstances = all.text.split()
-
-	//Sort the Project values (e.g. beachfront-dev, piazza-int) just for println readability.
-	def projects = allProjectValues.sort()
-	projects.each { p ->
-	    //The following returns one InstanceID value per line.
-	    def tagged = "aws ec2 describe-instances --filter Name=tag:Project,Values=$p --query Reservations[].Instances[].[InstanceId] --region us-west-2 --output text".execute()
-	    allTagged[p] = tagged.text.split()
-	}
-
-	def untaggedInstances = allInstances
-	projects.each { p ->
-	    allTagged[p].each { i ->
-		println "$i/$p"
-	    }
-	    untaggedInstances -= allTagged[p]
-	}
-	untaggedInstances.each { i ->
-	    println "$i/untagged"
+	    def allProjectValuesAsNameCRLFValue = []
+	    //The following returns a literal 'Project' on one line, then a value like 'beachfront-int' on the next line.
+            def getAllProjectValuesAsNameCRLFValue = "aws ec2 describe-instances --filter Name=tag-key,Values=Project --query Reservations[].Instances[].[Tags] --region $region --output text".execute()
+    	    allProjectValuesAsNameCRLFValue = getAllProjectValuesAsNameCRLFValue.text.split()
+    	    int allProjectValuesAsNameCRLFValueSize = allProjectValuesAsNameCRLFValue.size()
+    
+    	    def allProjectValues = []
+    	    for (int i=0; i<allProjectValuesAsNameCRLFValueSize; i += 2) {
+    	        if (allProjectValuesAsNameCRLFValue[i] == 'Project') {
+    		    allProjectValues.add(allProjectValuesAsNameCRLFValue[i+1])
+    	        }
+    	    }
+    
+    	    //Use a list of allInstances to determine which instancs are untagged,
+    	    // because the CLI doesn't seem to allow the same wildcards that the EC2 Dashboard GUI filter window allows
+    	    // e.g. "Not tagged" in "tag:Project : Not tagged"
+    	    def allInstances = []
+    	    def allTagged = [:]
+    	    //The following returns one InstanceId value per line.
+    	    def all = "aws ec2 describe-instances --query Reservations[].Instances[].[InstanceId] --region $region --output text".execute()
+    	    allInstances = all.text.split()
+    
+    	    //Sort the Project values (e.g. beachfront-dev, piazza-int) just for println readability.
+    	    def projects = allProjectValues.sort()
+    	    projects.each { p ->
+    	        //The following returns one InstanceID value per line.
+    	        def tagged = "aws ec2 describe-instances --filter Name=tag:Project,Values=$p --query Reservations[].Instances[].[InstanceId] --region $region --output text".execute()
+    	        allTagged[p] = tagged.text.split()
+    	    }
+    
+    	    def untaggedInstances = allInstances
+    	    projects.each { p ->
+    	        allTagged[p].each { i ->
+		    def ilong = (i.size() == 10)? i+'         ' : i
+    		    println "$region|$ilong|$p"
+    	        }
+    	        untaggedInstances -= allTagged[p]
+    	    }
+    	    untaggedInstances.each { i ->
+		def ilong = (i.size() == 10)? i+'         ' : i
+    	        println "$region|$ilong|untagged"
+    	    }
 	}
 
         render 'leaving servers5'
