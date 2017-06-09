e3582f1f4c376d2a0169a92373843a06e3aefcd0
EC2 Default User <ec2-user@ip-172-31-23-180.us-west-2.compute.internal>
2017-06-09 11:02:44 +0000

    Fix missing groovy script and related path

---
+++ maven3.groovy
@@ -0,0 +1,17 @@
+import jenkins.*
+
+import jenkins.model.*
+
+import hudson.*
+
+import hudson.model.*
+
+a=Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
+
+b=(a.installations as List);
+
+b.add(new hudson.tasks.Maven.MavenInstallation("MAVEN3", "/home/jenkins/apache-maven/apache-maven-3.2.3", []));
+
+a.installations=b
+
+a.save()
