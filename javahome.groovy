7e665177e7741af785e20ca9168d6b6e0e6f1b00
EC2 Default User <ec2-user@ip-172-31-31-187.us-west-2.compute.internal>
2017-06-10 22:18:44 +0000

    Config Jenkins JAVA_HOME via script

---
+++ javahome.groovy
@@ -0,0 +1,24 @@
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+  newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+  globalNodeProperties.add(newEnvVarsNodeProperty)
+  envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+  envVars = envVarsNodePropertyList.get(0).getEnvVars()
+
+}
+
+envVars.put("JAVA_HOME", "/usr/lib/jvm/java-1.7.0")
+
+instance.save()
